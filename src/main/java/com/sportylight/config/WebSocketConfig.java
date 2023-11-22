553e2b4fb58a9e9948b3646c0fd0b5386a795530
Mandu <kooo1225@naver.com>
2023-11-22 18:15:48 +0900

    Fix
    Done Config for WebSocket-STOMP

--- src/main/java/com/sportylight/config/WebSocketConfig.java
+++ src/main/java/com/sportylight/config/WebSocketConfig.java
@@ -1,18 +1,20 @@
 package com.sportylight.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-
 @Configuration
 @EnableWebSocketMessageBroker
+@ComponentScan(basePackages = { "com.sportylight.controller" })
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+    	System.out.println("Broker Load");
     	// 직접 구현한 웹소켓 핸들러
     	registry.enableSimpleBroker("/subscribe");
     	registry.setApplicationDestinationPrefixes("/app");
@@ -20,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-    	registry.addEndpoint("/endpoint").withSockJS();
+    	System.out.println("EndPoint Load");
+    	registry.addEndpoint("/endpoint").setAllowedOrigins("*").withSockJS();
     }
-  
 }
--- src/main/java/com/sportylight/controller/MessageController.java
+++ src/main/java/com/sportylight/controller/MessageController.java
@@ -2,9 +2,11 @@ package com.sportylight.controller;
 
 import java.util.Date;
 
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sportylight.domain.MessageVO;
@@ -12,10 +14,11 @@ import com.sportylight.domain.MessageVO;
 @Controller
 public class MessageController {
 	
-	@MessageMapping("/hello/{roomNo}")
-	@SendTo("/sub/message/{roomNo}")
-	public MessageVO sendMessage(MessageVO message) {
-		
+	@MessageMapping("/hello/room1")
+	@SendTo("/subscribe/message/room1")
+	public MessageVO sendMessage(MessageVO message, @Header("simpSessionId") String sessionId) {
+		System.out.println(message);
+		System.out.println("Received message ID : " + sessionId);
 		message.setSendDate(new Date());
 		
 		return message;
